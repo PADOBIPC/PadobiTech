735f1190ab5c56c050f6838818268b56bf78c139
IvanLaminat <6ahdut.72@gmail.com>
2025-10-16 00:00:53 +0300

    feat: setup database connection with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
+    TypeOrmModule.forRoot(databaseConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
