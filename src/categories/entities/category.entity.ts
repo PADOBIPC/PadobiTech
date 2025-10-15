9944e21dd2a7fd8b25f0cf196eba66a997365201
IvanLaminat <6ahdut.72@gmail.com>
2025-10-16 00:08:55 +0300

    feat: define entities and relationships

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,24 @@
-export class Category {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinColumn } from 'typeorm';
+import { Manufacturer } from '../../manufacturers/entities/manufacturer.entity';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity('categories')
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column('text') // 'text' подходит для длинных описаний
+  description: string;
+  
+  // Описываем связь: много категорий могут принадлежать одному производителю
+  @ManyToOne(() => Manufacturer, (manufacturer) => manufacturer.categories)
+  @JoinColumn({ name: 'manufacturerId' }) // Явно указываем имя колонки для связи
+  manufacturer: Manufacturer;
+
+  // Описываем связь: в одной категории может быть много продуктов
+  @OneToMany(() => Product, (product) => product.category)
+  products: Product[];
+}
--- src/manufacturers/entities/manufacturer.entity.ts
+++ src/manufacturers/entities/manufacturer.entity.ts
@@ -1,1 +1,26 @@
-export class Manufacturer {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Category } from '../../categories/entities/category.entity';
+import { Product } from '../../products/entities/product.entity';
+
+@Entity('manufacturers') // Указываем имя таблицы в БД
+export class Manufacturer {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true }) // Название производителя должно быть уникальным
+  name: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  foundedYear: number;
+
+  // Описываем связь: у одного производителя может быть много категорий
+  @OneToMany(() => Category, (category) => category.manufacturer)
+  categories: Category[];
+
+  // Описываем связь: у одного производителя может быть много продуктов
+  @OneToMany(() => Product, (product) => product.manufacturer)
+  products: Product[];
+}
