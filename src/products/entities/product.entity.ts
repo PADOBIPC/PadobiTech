f956b83e8024a5d9dc74d54c4d0ce9a8394ed85b
IvanLaminat <6ahdut.72@gmail.com>
2025-10-16 00:08:55 +0300

    feat: define entities and relationships

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,31 @@
-export class Product {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Manufacturer } from '../../manufacturers/entities/manufacturer.entity';
+import { Category } from '../../categories/entities/category.entity';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('decimal', { precision: 10, scale: 2 }) // Для хранения цен
+  price: number;
+  
+  @Column()
+  stock: number; // Количество на складе
+
+  // Связь с производителем
+  @ManyToOne(() => Manufacturer, (manufacturer) => manufacturer.products)
+  @JoinColumn({ name: 'manufacturerId' })
+  manufacturer: Manufacturer;
+
+  // Связь с категорией
+  @ManyToOne(() => Category, (category) => category.products)
+  @JoinColumn({ name: 'categoryId' })
+  category: Category;
+}
