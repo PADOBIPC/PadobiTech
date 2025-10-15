070fe2a3c057fcbb99b147b47b50a74b70d877c9
IvanLaminat <6ahdut.72@gmail.com>
2025-10-16 00:03:48 +0300

    feat: generate resources for manufacturers, categories, products

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { ConfigModule } from '@nestjs/config';
 import { databaseConfig } from './config/database.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ManufacturersModule } from './manufacturers/manufacturers.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
-    TypeOrmModule.forRoot(databaseConfig),
+    TypeOrmModule.forRoot(databaseConfig), ManufacturersModule, CategoriesModule, ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
