2515d8945400d12690e734f21bbd6a68e7f6fa6b
IvanLaminat <6ahdut.72@gmail.com>
2025-10-21 13:50:40 +0300

    feat: Добавил профиль текущего пользователя

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
@@ -9,5 +11,14 @@ export class UsersController {
   @Post('/register')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Req() req) {
+    const user: User = req.user;
+    
+    const { password, ...result } = user; 
+    
+    return result;
   }
 }
